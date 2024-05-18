cac604869bfe1275769a021bee27353ac0e754f5
Allan Kizito <allan.k@ametnes.com>
2024-05-18 17:33:27 -0400

    Google logout process

--- nesis/frontend/client/src/SessionContext.js
+++ nesis/frontend/client/src/SessionContext.js
@@ -11,6 +11,7 @@ import { useHistory } from 'react-router-dom';
 import useSessionStorage from './utils/useSessionStorage';
 import apiClient from './utils/httpClient';
 import { PublicClientApplication } from '@azure/msal-browser';
+import { googleLogout } from '@react-oauth/google';
 
 const SessionContext = React.createContext({
   session: null,
@@ -67,6 +68,7 @@ export function useSignOut(client, config) {
       clearToken();
       setSession(null);
       logoutMicrosoft(config);
+      logoutGoogle();
       history.push('/signin');
     },
     [setSession, history],
@@ -88,6 +90,10 @@ async function logoutMicrosoft(config) {
   } catch (e) {
     /* ignored */
   }
+}
+
+async function logoutGoogle() {
+  googleLogout();
 }
 
 function logoutNesis(client) {
