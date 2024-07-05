1aafc21f181c33866364a8a8c5aa128a75a37cd1
Michael Sekamanya <mawandm@gmail.com>
2024-07-05 15:41:14 -0700

    Upgrade tokenizers

--- package.sh
+++ package.sh
@@ -1,4 +1,4 @@
 version=$1
-docker build --build-arg NESIS_VERSION="$version" -t ametnes/nesis:"$version"-api . -f nesis/api/Dockerfile
-docker build --build-arg NESIS_VERSION="$version" --build-arg PUBLIC_URL=/ --build-arg PROFILE=PROD -t ametnes/nesis:"$version"-frontend . -f nesis/frontend/Dockerfile
+#docker build --build-arg NESIS_VERSION="$version" -t ametnes/nesis:"$version"-api . -f nesis/api/Dockerfile
+#docker build --build-arg NESIS_VERSION="$version" --build-arg PUBLIC_URL=/ --build-arg PROFILE=PROD -t ametnes/nesis:"$version"-frontend . -f nesis/frontend/Dockerfile
 docker build --build-arg NESIS_VERSION="$version" -t ametnes/nesis:"$version"-rag . -f nesis/rag/Dockerfile
